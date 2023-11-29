4f01746b4ffffcda68561155bc182c6790403077
Kusaka <v.gerasimov.sas@gmail.com>
2023-11-29 19:33:17 +0200

    main: add include guards for GLFW

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <stdexcept>
 
+#ifndef GLFW
+#define GLFW
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#endif //GLFW
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
