a042aed5eece9a35cf13856e416dbb9647b00f16
Kusaka <v.gerasimov.sas@gmail.com>
2023-12-03 15:43:13 +0200

    input_manager: add comments, change formatting

--- include/input_manager.h
+++ include/input_manager.h
@@ -16,6 +16,7 @@
 
 namespace ale {
 
+// An action executed by the editor
 enum InputAction {
     CAMERA_MOVE_F,
     CAMERA_MOVE_B,
@@ -27,21 +28,25 @@ enum InputAction {
 
 class InputManager {
 private:
+    // NOTE: [03.12.2023] I use `map` instead of `unordered_map` for better
+    // traversal time complexity and cache coherency.
+    // It is possible to use a `vector` to store keys of the `unordered_map`, 
+    // but now readability is more important than O(1)
 
-    // A map of (KEY,ACTION) bindings (not an unordered_map for better traversal)
+    // A map of (KEY,ACTION) bindings
     std::map<int, InputAction> _inputBindings;
     // A map for key shortcuts and text inputs (uses callbacks)
     std::map<InputAction, std::function<void()> > _callbackBindings;
     // A map for continuous input (uses runtime checks)
     std::map<InputAction, std::function<void()> > _functionContBindings;
-    
+
+    // TODO: This is a raw pointer. Get rid of it later
     GLFWwindow* window_p;
     
     void _bindKey(int key, InputAction action);
     static void _keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods);
     void _executeAction(InputAction _action);
     bool _isKeyPressed(GLFWwindow *window, int key);
-    
 
 public:
     InputManager();
@@ -57,6 +62,7 @@ InputManager::InputManager() {}
 
 InputManager::~InputManager() {}
 
+// TODO: It is probably a good idea to move this to the constructor
 void InputManager::init(GLFWwindow* window) {
     window_p = window;
     glfwSetWindowUserPointer(window, this);
@@ -92,7 +98,13 @@ void InputManager::_bindKey(int key, InputAction action) {
     _inputBindings[key] = action;
 }
 
-// TODO: implement a more verboose distinction between input methods
+bool InputManager::_isKeyPressed(GLFWwindow* window, int key){
+    return glfwGetKey(window, key);
+}
+
+// Binds a lambda funciton to an editor action. For now the function should have
+// a declaration `void func_name(void)`, but it can use context data
+// TODO: implement a more verbose distinction between input methods (i.e. enum)
 void InputManager::bindFunction(InputAction _action, std::function<void()> _function, bool isContinuous) {    
     if (isContinuous) {
         _functionContBindings[_action] = _function;
@@ -101,15 +113,12 @@ void InputManager::bindFunction(InputAction _action, std::function<void()> _func
     }
 }
 
-bool InputManager::_isKeyPressed(GLFWwindow* window, int key){
-    return glfwGetKey(window, key);
-} 
-
 bool InputManager::isActionActive(InputAction _action){
     return false;
 } 
 
-//allows to execute active continuous actions (like camera controls)
+// Allows to execute active continuous actions (like camera controls)
+// Should be called from the main loop/input thread
 bool InputManager::executeActiveActions() {
     for (auto binding:_inputBindings) {
         if (_isKeyPressed(window_p, binding.first)) {
