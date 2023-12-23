4fd374e977ee9cb1019e1cfa9f9d1a890b4b63ae
Kusaka <v.gerasimov.sas@gmail.com>
2023-12-23 17:28:06 +0200

    re_mesh: add geometry structs

--- include/re_mesh.h
+++ include/re_mesh.h
@@ -3,58 +3,65 @@
 /* 
     This is a mesh implementation that uses Radial Edge Structure for 
     fast manipulation of non-manifold meshes.
+
+    Main inspiration and reference for this data structure: 
+    https://wiki.blender.org/wiki/Source/Modeling/BMesh/Design
 */
 
 namespace ale {
+// geo namespace defines geometry primitives
+namespace geo {
 
+// TODO: this is a boilerplate mesh class, it must be extended
+class REMesh {    
+public:
+    std::vector<Face> faces;
+    std::vector<Edge> edges;
+    std::vector<Vertex> vertices;
+};
 
-class REMesh {
+struct Vertex;
+struct Edge;
+struct Loop;
+struct DiskLink;
+struct Face;
 
+struct Vertex {
+    glm::vec3 pos;
+    glm::vec3 color;
+    glm::vec2 texCoord;
+    // An edge in the disk loop
+    std::shared_ptr<Edge> edge;
+};
+
+struct Edge {
+    // Origin and destination vertices
+    std::shared_ptr<Vertex> v1, v2;
+    std::shared_ptr<Loop> loop;
 };
 
 
-// Generic class for geometry primitives
-class Geometry {
-
+// Loop node around the face
+struct Loop {
+    std::shared_ptr<Vertex> v;
+    std::shared_ptr<Vertex> e;
+    // The face the loop belongs to
+    std::shared_ptr<Vertex> f;
+    std::shared_ptr<DiskLink> prev, next;
+    
 };
 
-class Edge : Geometry {
-
+// Node of a linked list that loops around edges
+struct DiskLink {
+    std::shared_ptr<Edge> prev, next;    
 };
 
-class Face : Geometry {
-
-};
-
-class Vetex : Geometry {
-
-};
-
-class Loop : Geometry {
-
+struct Face {    
+    // A pointer to the first loop node
+    std::shared_ptr<Loop> loop;    
+    glm::vec3 nor;
 };
 
 
+} // namespace geo
 } // namespace ale
-
-/* 
-This is a draft for mesh classes. May translate to an UML diagram in the future.
-Will be update as I learn about REDS.
-
-Vertex:
-    position as a glm vector
-    pointer to one of the adjacent edges
-
-Edge:
-    two adjacent vertices
-    link to a list of loops in a radial cycle
-
-Face:
-    first loop in a loop cycle
-
-Loop:
-    edge
-    vertex at the start of edge
-    face
-
-*/
