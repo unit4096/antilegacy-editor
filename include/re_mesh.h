4fc9a9eba5f8f53a8d49343c9738c7cb4c40314a
Kusaka <v.gerasimov.sas@gmail.com>
2023-12-24 00:02:39 +0200

    re_mesh: fix errors, add include guards, remove dependency from primitives.h

--- include/re_mesh.h
+++ include/re_mesh.h
@@ -1,12 +1,32 @@
-#include <primitives.h>
-
 /* 
     This is a mesh implementation that uses Radial Edge Structure for 
-    fast manipulation of non-manifold meshes.
+    fast manipulation of non-manifold meshes. It omits the shell structure since
+    solid modeling is out of scope of the editor.
 
     Main inspiration and reference for this data structure: 
     https://wiki.blender.org/wiki/Source/Modeling/BMesh/Design
+    [23.12.2023] Source code of the original data structure (well-documented!):
+    https://github.com/blender/blender/blob/main/source/blender/bmesh/bmesh_class.hh
+
 */
+
+#ifndef ALE_REMESH
+#define ALE_REMESH
+
+// ext
+#pragma once
+#include <vector>
+#include <memory>
+
+#ifndef GLM
+#define GLM
+
+#include <glm/glm.hpp>
+
+#endif // GLM
+
+
+
 
 namespace ale {
 // geo namespace defines geometry primitives
@@ -38,17 +58,20 @@ struct Edge {
     // Origin and destination vertices
     std::shared_ptr<Vertex> v1, v2;
     std::shared_ptr<Loop> loop;
+    std::shared_ptr<DiskLink> v1_disk, v2_disk;
 };
 
 
 // Loop node around the face
 struct Loop {
     std::shared_ptr<Vertex> v;
-    std::shared_ptr<Vertex> e;
+    std::shared_ptr<Edge> e;
     // The face the loop belongs to
-    std::shared_ptr<Vertex> f;
-    std::shared_ptr<DiskLink> prev, next;
-    
+    std::shared_ptr<Face> f;
+    // Loops connected to the edge
+    std::shared_ptr<Loop> radial_prev, radial_next;
+    // Loops forming a face
+    std::shared_ptr<Loop> prev, next;
 };
 
 // Node of a linked list that loops around edges
@@ -65,3 +88,5 @@ struct Face {
 
 } // namespace geo
 } // namespace ale
+
+#endif // ALE_REMESH
