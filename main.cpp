76aa9291117bc044ef805fc6228faf1898e1eb90
Kusaka <v.gerasimov.sas@gmail.com>
2023-12-29 13:43:14 +0200

    main: update include guards

--- main.cpp
+++ main.cpp
@@ -14,14 +14,14 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_ENABLE_EXPERIMENTAL
 
-#ifndef IMGUI
-#define IMGUI
+#ifndef ALE_IMGUI
+#define ALE_IMGUI
 
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_vulkan.h>
 
-#endif //IMGUI
+#endif //ALE_IMGUI
 
 // int
 #include <input_manager.h>
