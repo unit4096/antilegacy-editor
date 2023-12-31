7345e266d05153b66ecfc4cbad5bf2baebe0f0f1
Kusaka <v.gerasimov.sas@gmail.com>
2023-12-31 14:08:23 +0200

    renderer: remove imgui include guards

--- include/renderer.h
+++ include/renderer.h
@@ -44,15 +44,11 @@ development, renederer code still somewhat resembles the original tutorial code.
 #include <primitives.h>
 #include <camera.h>
 
-#ifndef ALE_IMGUI
-#define ALE_IMGUI
-
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_vulkan.h>
 #include <imgui/ImGuizmo.h>
 
-#endif //ALE_IMGUI
 
 
 namespace ale {
