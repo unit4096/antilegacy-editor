001a130e785c143eb2644fc185be7a67f8890dad
Kusaka <v.gerasimov.sas@gmail.com>
2024-01-11 23:21:35 +0200

    vk_init: add header for vulkan initialization heplers

---
+++ include/vk_init.h
@@ -0,0 +1,15 @@
+/* 
+    This file contains helper funcitons for vulkan structures. Its main purpose
+    is to make renderer.h less verboose
+*/
+
+namespace ale {
+namespace vkinit {
+
+
+
+
+
+    
+} // namespace vkinit
+} // namespace ale
