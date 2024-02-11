b324874e34d2deed2c520c4d07377160696eda2b
Kusaka <v.gerasimov.sas@gmail.com>
2024-02-11 15:29:44 +0200

    geometry_utils: add file for future geometry functions

---
+++ include/geometry_utils.h
@@ -0,0 +1,25 @@
+//ext
+#ifndef GLM
+#define GLM
+
+#include <glm/glm.hpp>
+
+#endif // GLM
+
+//int
+#include <primitives.h>
+
+#ifndef ALE_GEO_UTILS
+
+namespace ale {
+namespace geo {
+
+
+
+
+} // namespace geo
+} // namespace ale
+
+
+#define ALE_GEO_UTILS
+#endif //ALE_GEO_UTILS
