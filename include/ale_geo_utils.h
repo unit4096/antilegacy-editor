ef42e9bf9f94a87adf54639edd7fa052f319b90a
Kusaka <v.gerasimov.sas@gmail.com>
2024-02-19 15:56:02 +0200

    ale_geo_utils: fix edge cases in addLoopToEdge, remove unteted functions

--- include/ale_geo_utils.h
+++ include/ale_geo_utils.h
@@ -26,27 +26,64 @@ namespace trc = ale::Tracer;
 namespace ale {
 namespace geo {
 
-// Get Face bounding loops
+/*
+    Following functions manage REMesh primitives.
+*/
+
+// Append a geo::Loop to the geo::Edge's radial loop cycle
+[[maybe_unused]]
+static void addLoopToEdge(geo::Edge* e, geo::Loop* l) {
+    assert(e);
+    assert(l);
+    // If an edge does not have a loop, add l as e->loop
+    // and make l loop to itself
+    if (!e->loop) {
+        l->radial_next = l;
+        l->radial_prev = l;
+        e->loop = l;
+        return;
+    }
+
+    auto _l = e->loop;
+    // e->loop loops to itself
+    if (_l->radial_next == _l) {
+        // Make e->loop to loop back to l
+        l->radial_next = _l;
+        l->radial_prev = _l;
+        _l->radial_next = l;
+        _l->radial_prev = l;
+        return;
+    }
+    // There is a radial cycle and it is not just one loop
+
+
+    // Insert l between e->loop and its radial_next
+    l->radial_prev = _l;
+    l->radial_next = _l->radial_next;
+    _l->radial_next->radial_prev = l;
+    _l->radial_next = l;
+}
+
+// Get bounding loops of geo::Face
 [[maybe_unused]]
 static bool getBoundingLoops(const Face* face,
                       std::vector<Loop*>& out_loops) {
 
-    // The vert is not attached to any edge. Something is
-    // very wrong with your topology
-
-
+    assert(face->size >= 3);
     if (face->loop == nullptr) {
         // This edge does not belong to any face
         return false;
     }
-
-    out_loops = {};
     auto l = face->loop;
     auto lItr = l;
     assert(lItr->next);
+    // Reset out_ variable
+    out_loops = {};
+
     out_loops.push_back(lItr);
     lItr = lItr->next;
 
+    // Iterate over the loop cycle
     while (lItr != l) {
         assert(lItr->next != nullptr);
         out_loops.push_back(lItr);
@@ -56,119 +93,6 @@ static bool getBoundingLoops(const Face* face,
     return true;
 }
 
-/*
-
-// Edge cycles are a collection of loops connected to the sides
-// of the current loop
-// They are unordered
-static void appenLoopToRadialLoopCycle(geo::Edge* e, geo::Loop* new_l) {
-    auto old_l = e->loop;
-    if (old_l->radial_next == nullptr && old_l->radial_prev == nullptr) {
-        old_l->radial_prev = new_l;
-        old_l->radial_next = new_l;
-    }
-    old_l->radial_next->prev = new_l;
-    old_l->radial_next = new_l;
-};
-static bool dHasE(geo::Disk* d, geo::Edge* e) {
-    return d->next == e || d->prev == e;
-};
-
-static auto getNextDInLoop(geo::Edge* e, sp<geo::Disk* d) {
-    trc::raw << &d << " ";
-    assert(e);
-    assert(d);
-    geo::Disk> res = nullptr;
-
-    auto next = d->next;
-
-    if (next->d1->prev == e) {
-        res = next->d1;
-        return res;
-    }
-
-    if (next->d2->prev == e) {
-        res = next->d2;
-        return res;
-    }
-
-    trc::log("NO BACK LINK FOUND!", trc::ERROR);
-    return res;
-};
-
-static bool vLoopHasE(geo::Vert> v, sp<geo::Edge> e) {
-    assert(e);
-    assert(v->edge);
-
-    // If an edge exists, it should have a link to at least one vertex
-    assert(v->edge->d1 || v->edge->d2);
-
-
-    auto _e = v->edge;
-    if (_e == e) {
-        return true;
-    }
-
-    auto d = _e->d1;
-
-    if (!isV1InE(v,_e)) {
-        d = _e->d2;
-    }
-
-    auto eItr = _e;
-    auto dItr = d;
-
-    if (d->next == e) {
-        return true;
-    }
-
-    dItr = getNextDInLoop(eItr, dItr);
-
-    int limit = 5;
-
-    while (dItr->next != _e) {
-        if (limit == 0) {
-            trc::log("LiMIT REACHED! ", trc::ERROR);
-            return false;
-        }
-
-        if (dItr->next == e) {
-            return true;
-        }
-
-        dItr = getNextDInLoop(eItr, dItr);
-        eItr = dItr->prev;
-        limit--;
-    }
-
-    return false;
-};
-
-static void addEToVertL(geo::Vert> v, sp<geo::Edge> e_new) {
-    if (geo::isV1InE(v,e_new)) {
-        geo::addEToD1(v->edge, e_new);
-    } else {
-        geo::addEToD2(v->edge, e_new);
-    }
-}
-
-*/
-
-// Append a geo::Loop to the geo::Edge's radial loop cycle
-[[maybe_unused]]
-static void addLoopToEdge(geo::Edge* e, geo::Loop* l) {
-    if (!e->loop) {
-        l->radial_next = l;
-        l->radial_prev = l;
-        e->loop = l;
-        return;
-    }
-
-    auto _l = e->loop;
-    _l->radial_prev->radial_next = l;
-    _l->radial_prev = l;
-    e->loop = l;
-}
 
 // https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
 // Checks intersection of a ray and a triangle
@@ -198,18 +122,24 @@ static bool rayIntersectsTriangle(const glm::vec3& rayOrigin,
     float det = glm::dot(edge1, ray_cross_e2);
 
     // This ray is parallel to this triangle.
-    if (det > -epsilon && det < epsilon) return false;
+    if (det > -epsilon && det < epsilon) {
+        return false;
+    }
 
     float inv_det = 1.0 / det;
     glm::vec3 s = rayOrigin - a;
     float u = inv_det * glm::dot(s, ray_cross_e2);
 
-    if (u < 0 || u > 1) return false;
+    if (u < 0 || u > 1) {
+        return false;
+    }
 
     glm::vec3 s_cross_e1 = glm::cross(s, edge1);
     float v = inv_det * glm::dot(rayDir, s_cross_e1);
 
-    if (v < 0 || u + v > 1) return false;
+    if (v < 0 || u + v > 1) {
+        return false;
+    }
 
     // At this stage we can compute t to find out
     // where the intersection point is on the line.
