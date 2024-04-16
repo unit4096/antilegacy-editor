d9806131517e703e78a0488a1c43d4528dda1dfa
Kusaka <v.gerasimov.sas@gmail.com>
2024-04-16 21:41:18 +0300

    imgui_gradient: add gradient buttons

--- include/ale_imgui_interface.h
+++ include/ale_imgui_interface.h
@@ -2,9 +2,12 @@
 #define ALE_IMGUI
 #pragma once
 
+
+#define IMGUI_DEFINE_MATH_OPERATORS
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_vulkan.h>
+#include <imgui/imgui_gradient.h>
 #include <imgui/ImGuizmo.h>
 
 #endif //ALE_IMGUI
